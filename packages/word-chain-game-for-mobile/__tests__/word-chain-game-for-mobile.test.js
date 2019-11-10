ab21df3f04196efb7c241af0138a36043b391157
natari <triplfjump1417@gmail.com>
2019-11-10 21:42:47 +0900

    flutter project 追加

--- packages/word-chain-game-for-mobile/__tests__/word-chain-game-for-mobile.test.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-const wordChainGameForMobile = require('..');
-
-describe('word-chain-game-for-mobile', () => {
-    it('needs tests');
-});
--- packages/word-chain-game-for-mobile/lib/word-chain-game-for-mobile.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-module.exports = wordChainGameForMobile;
-
-function wordChainGameForMobile() {
-    // TODO
-}
